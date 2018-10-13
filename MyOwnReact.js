b9435d46c1ac31ebc401ff3e2b240c26b47ec204
romanhavr <romanhavr@ukr.net>
2018-10-13 14:29:21 +0300

    lesson 1

---
+++ MyOwnReact.js
@@ -0,0 +1,52 @@
+const React = {
+	createElement: (type, props, ...children) => {
+		var node = transformInput(type, props, ...children);
+		createElementNode(node)
+	},
+	render: (a, el) => {
+		return el.appendChild(a);
+	},
+};
+
+let i = 1;
+
+const app = 
+	React.createElement('div', { style: { backgroundColor: 'red' } }, [
+						React.createElement('span', undefined, 'Hello world'),
+						React.createElement('br'),
+						'This is just a text node',
+						React.createElement('div', { textContent: 'Text content' }), 
+	]);
+
+React.render(
+	app,
+	document.getElementById('root'),
+);
+
+function createElementNode(node) {
+	if (node) {
+		if (typeof node === 'string') {
+			return document.createTextNode(node);
+		};
+		if (typeof node.type !== 'undefined') {
+			const element = document.createElement(node.type);
+			if (node.props) {
+				Object.keys(node.props).forEach(name => {
+					element.setAttribute(name, node.props[name]);
+				})
+			};
+			if (typeof node.children !== 'undefined') { 
+				for (let j = 0; j <= node.children.length; j++) {
+					createElementNode(node.children[j]);
+					element.appendChild.bind(element);
+				}
+			};
+			i++;
+			return element;
+		}
+	}
+};
+
+function transformInput(type, props, ...children) {
+	return { type, props: props || {}, children };
+};
